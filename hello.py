31ee376fdfc786f7da9508eab100116854cdd04d
ecortes91 <98487402+ecortes91@users.noreply.github.com>
2024-09-13 16:49:27 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,6 +2,11 @@
 
 def main():
     print("cambiando algo")
+    target = -5
+    num = 3
+
+    target =- num  # Noncompliant: target = -3. Is that really what's meant?
+    target =+ num # Noncompliant: target = 3
 
 if __name__ == "__main__":
     main()
